5b7d2d9f993e56a04421fd471ac3f2d83938bbed
IV80 <shelly1581114@gmail.com>
2024-08-06 17:02:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,15 +13,15 @@ async function fetchRecommendedVideos() {
 }
 
 function displayVideos(videos) {
-    const videoContainer = document.querySelector('.content');
+    const videoContainer = document.querySelector('.video-list');
     videoContainer.innerHTML = ''; // Очистка контейнера перед добавлением новых видео
 
     videos.forEach(video => {
         const videoElement = document.createElement('div');
         videoElement.classList.add('video-item');
         videoElement.innerHTML = `
-            <h3>${video.snippet.title}</h3>
             <img src="${video.snippet.thumbnails.medium.url}" alt="${video.snippet.title}">
+            <h3>${video.snippet.title}</h3>
             <p>${video.snippet.description}</p>
         `;
         videoContainer.appendChild(videoElement);
